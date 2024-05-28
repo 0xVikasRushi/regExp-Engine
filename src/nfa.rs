3babb3c6182e7fd9b7728362be7a2f958db5108e
0xvikasrushi <0xvikas@gmail.com>
2024-05-28 20:40:19 +0530

    fix: fk Interior Mutability

--- src/nfa.rs
+++ src/nfa.rs
@@ -1,37 +1,37 @@
 use crate::state::{State, EPSILON};
+use std::cell::RefCell;
+use std::rc::Rc;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct NFA {
-    pub in_state: State,
-    pub out_state: State,
+    pub in_state: Rc<RefCell<State>>,
+    pub out_state: Rc<RefCell<State>>,
 }
+
 impl NFA {
     pub fn new() -> NFA {
         NFA {
-            in_state: State::new(false),
-            out_state: State::new(true),
+            in_state: Rc::new(RefCell::new(State::new(false))),
+            out_state: Rc::new(RefCell::new(State::new(true))),
         }
-    }
-    pub fn test() -> bool {
-        false
     }
 
     pub fn char(symbol: &str) -> NFA {
-        let mut nfa = NFA::new();
-        nfa.out_state.accepting = true;
-
+        let nfa = NFA::new();
+        nfa.out_state.borrow_mut().accepting = true;
         nfa.in_state
+            .borrow_mut()
             .add_transition_for_symbol(symbol, nfa.out_state.clone());
-
-        return nfa;
+        nfa
     }
 
     pub fn concat_pair(first: &mut NFA, second: &mut NFA) -> NFA {
-        first.out_state.accepting = false;
-        second.out_state.accepting = true;
+        first.out_state.borrow_mut().accepting = false;
+        second.out_state.borrow_mut().accepting = true;
 
         first
             .out_state
+            .borrow_mut()
             .add_transition_for_symbol(EPSILON, second.in_state.clone());
 
         NFA {
@@ -50,31 +50,11 @@ impl NFA {
         current_nfa
     }
 }
+
 #[cfg(test)]
 mod test {
-
     use super::*;
     use crate::state::EPSILON;
-
-    #[test]
-    fn test_concat() {
-        let mut first = NFA::char("a");
-        let second = NFA::char("b");
-        let third = NFA::char("c");
-
-        let mut array_of_nfa = vec![second, third];
-
-        let final_nfa = NFA::concat(&mut first, &mut array_of_nfa);
-
-        assert_eq!(final_nfa.in_state.accepting, false);
-        assert_eq!(final_nfa.out_state.accepting, true);
-
-        let first_in_state = &final_nfa.in_state;
-        let first_out_state = &final_nfa.in_state;
-
-        assert_eq!(*first_in_state, first.in_state);
-        assert_eq!(*first_out_state, first.out_state);
-    }
 
     #[test]
     fn test_concat_pair() {
@@ -83,25 +63,49 @@ mod test {
 
         let combine_transition = NFA::concat_pair(&mut first, &mut second);
 
-        assert_eq!(first.in_state.accepting, false);
-        assert_eq!(first.out_state.accepting, false);
+        assert_eq!(first.in_state.borrow().accepting, false);
+        assert_eq!(first.out_state.borrow().accepting, false);
 
-        assert_eq!(second.in_state.accepting, false);
-        assert_eq!(second.out_state.accepting, true);
+        assert_eq!(second.in_state.borrow().accepting, false);
+        assert_eq!(second.out_state.borrow().accepting, true);
 
-        assert_eq!(combine_transition.in_state.accepting, false);
-        assert_eq!(combine_transition.out_state.accepting, true);
+        assert_eq!(combine_transition.in_state.borrow().accepting, false);
+        assert_eq!(combine_transition.out_state.borrow().accepting, true);
 
-        let epslion_transition = first.out_state.get_transition_for_symbol(EPSILON);
-        assert_eq!(epslion_transition.len(), 1);
+        let epsilon_transition = first.out_state.borrow().get_transition_for_symbol(EPSILON);
+        assert_eq!(epsilon_transition.len(), 1);
 
-        let epslion_transition_state = epslion_transition.get(0).unwrap();
-        assert_eq!(*epslion_transition_state, second.in_state);
+        let epsilon_transition_state = epsilon_transition.get(0).unwrap();
+        assert_eq!(
+            *epsilon_transition_state.borrow(),
+            *second.in_state.borrow()
+        );
 
-        let b_transition = second.in_state.get_transition_for_symbol("b");
+        let b_transition = second.in_state.borrow().get_transition_for_symbol("b");
         assert_eq!(b_transition.len(), 1);
 
         let b_expected_out_state = b_transition.get(0).unwrap();
-        assert_eq!(*b_expected_out_state, second.out_state);
+        assert_eq!(*b_expected_out_state.borrow(), *second.out_state.borrow());
+    }
+
+    #[test]
+    fn test_concat() {
+        let mut first = NFA::char("a");
+        let second = NFA::char("b");
+        let third = NFA::char("c");
+
+        dbg!(first.clone());
+        let mut array_of_nfa = vec![second, third];
+
+        let final_nfa = NFA::concat(&mut first, &mut array_of_nfa);
+
+        assert_eq!(final_nfa.in_state.borrow().accepting, false);
+        assert_eq!(final_nfa.out_state.borrow().accepting, true);
+
+        assert_eq!(*final_nfa.in_state.borrow(), *first.in_state.borrow());
+        assert_eq!(
+            *final_nfa.out_state.borrow(),
+            *array_of_nfa.last().unwrap().out_state.borrow()
+        );
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -1,11 +1,11 @@
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 pub const EPSILON: &str = "ε";
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct State {
     pub accepting: bool,
-    pub transition_map: HashMap<String, Vec<State>>,
+    pub transition_map: HashMap<String, Vec<Rc<RefCell<State>>>>,
 }
 
 impl State {
@@ -16,39 +16,41 @@ impl State {
         }
     }
 
-    pub fn add_transition_for_symbol(&mut self, symbol: &str, new_state: State) {
+    pub fn add_transition_for_symbol(&mut self, symbol: &str, new_state: Rc<RefCell<State>>) {
         self.transition_map
             .entry(symbol.to_string())
             .or_insert_with(Vec::new)
             .push(new_state);
     }
 
-    pub fn get_transition_for_symbol(&self, symbol: &str) -> Vec<State> {
+    pub fn get_transition_for_symbol(&self, symbol: &str) -> Vec<Rc<RefCell<State>>> {
         match self.transition_map.get(symbol) {
             Some(states) => states.clone(),
             None => Vec::new(),
         }
     }
 }
-
 #[cfg(test)]
 mod test {
 
     use super::*;
+    use std::cell::RefCell;
+    use std::rc::Rc;
 
     #[test]
     fn test_add_and_get_transition() {
-        let mut s1 = State::new(false);
-        let s2 = State::new(true);
+        let s1 = Rc::new(RefCell::new(State::new(false)));
+        let s2 = Rc::new(RefCell::new(State::new(true)));
 
-        s1.add_transition_for_symbol(EPSILON, s2.clone());
-        let transition_table_for_a = &s1.get_transition_for_symbol(EPSILON);
+        s1.borrow_mut()
+            .add_transition_for_symbol(EPSILON, s2.clone());
+        let transition_table_for_epsilon = s1.borrow().get_transition_for_symbol(EPSILON);
 
-        let first_state = transition_table_for_a.get(0);
+        let first_state = transition_table_for_epsilon.get(0);
         match first_state {
             Some(state) => {
-                assert_eq!(&s2, state);
-                assert_eq!(s2.accepting, true);
+                assert_eq!(*state.borrow(), *s2.borrow());
+                assert_eq!(s2.borrow().accepting, true);
             }
             None => {
                 panic!("No state found in transition table");
@@ -58,34 +60,36 @@ mod test {
 
     #[test]
     fn test_multiple_transitions() {
-        let mut s1 = State::new(false);
-        let mut s2 = State::new(false);
-        let s3 = State::new(true);
+        let s1 = Rc::new(RefCell::new(State::new(false)));
+        let s2 = Rc::new(RefCell::new(State::new(false)));
+        let s3 = Rc::new(RefCell::new(State::new(true)));
 
-        s1.add_transition_for_symbol(EPSILON, s2.clone());
+        s1.borrow_mut()
+            .add_transition_for_symbol(EPSILON, s2.clone());
 
-        let transition_table_for_a = s1.get_transition_for_symbol(EPSILON);
-        assert_eq!(transition_table_for_a.len(), 1);
+        let transition_table_for_epsilon = s1.borrow().get_transition_for_symbol(EPSILON);
+        assert_eq!(transition_table_for_epsilon.len(), 1);
 
-        let first_state = transition_table_for_a.get(0);
+        let first_state = transition_table_for_epsilon.get(0);
         match first_state {
             Some(state) => {
-                assert_eq!(state.accepting, false);
+                assert_eq!(state.borrow().accepting, false);
             }
             None => {
                 panic!("No state found in transition table");
             }
         }
-        s2.add_transition_for_symbol(EPSILON, s3.clone());
 
-        let transition_table_for_b = &s2.get_transition_for_symbol(EPSILON);
+        s2.borrow_mut()
+            .add_transition_for_symbol(EPSILON, s3.clone());
 
-        assert_eq!(transition_table_for_b.len(), 1);
+        let transition_table_for_s2_epsilon = s2.borrow().get_transition_for_symbol(EPSILON);
+        assert_eq!(transition_table_for_s2_epsilon.len(), 1);
 
-        let second_state = transition_table_for_b.get(0);
+        let second_state = transition_table_for_s2_epsilon.get(0);
         match second_state {
             Some(state) => {
-                assert_eq!(state.accepting, true);
+                assert_eq!(state.borrow().accepting, true);
             }
             None => {
                 panic!("No state found in transition table");
